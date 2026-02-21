458d0a045a5b659349379f1815fce8bce18cd984
Paul5678MG <montoyapaul2005@gmail.com>
2026-02-20 23:30:23 -0500

    Ejercicios 4 y 5 de python agregados

--- ejecicios_python_1.py
+++ ejecicios_python_1.py
@@ -71,3 +71,59 @@ print(f"IPs extraídas: {lista_ips}")
 pattern1=r"USR:([^_]+)_IP"
 cadena_editada=re.sub(pattern1,"USR:[REDACTED]_IP",cadena)
 print(f"Cadena editada: {cadena_editada}")
+
+#4)
+tokens_activos={
+    "TK-96":"ADMIN",
+    "TK-97":"USER",
+    "TK-98":"USER",
+    "TK-99":"GUEST"
+}
+
+class TokenInvalido(Exception):
+    def __init__(self,err):
+        print(f"Error: {err}")
+
+def verificar_acceso(token_id):
+    try:
+        if token_id in tokens_activos:
+            if tokens_activos[token_id]=="ADMIN":
+                print("[ACCESO TOTAL] Bienvenido, Paul.")
+            elif tokens_activos[token_id]=="GUEST":
+                print("[ACCESO LIMITADO] Solo lectura.")
+            elif tokens_activos[token_id]=="USER":
+                print("[ACCESO COMÚN] Lectura y edición")
+        else:
+            raise TokenInvalido("[ALERTA] Token inválido detectado.")
+    except TokenInvalido as e:
+        print(f"Details: {type(e).__name__}")
+    else:
+        print("No hubo tokens invalidos.")
+    finally:
+        print("Verificación completada")
+
+verificar_acceso("TK-96")
+
+#5)
+import re
+
+def extraer_vulnerabilidades(ruta):
+    with open(ruta, "r",encoding="UTF-8") as file:
+        text=file.read()
+        
+        pattern=r"CVE{1}\-{1}\d{4}\-\d{4}"
+        
+        codigos=re.findall(pattern,text)
+        print(f"Codigos: {codigos}")
+        
+        codigos_unicos=set(codigos)
+        print(f"Codigos unicos: {codigos_unicos}")
+        
+        with open("ejercicios_repaso_FINAL\\extraccion.txt", "w",encoding="UTF-8") as file:
+            file.write(text)
+            file.write("\n----------------------------------\n")
+            file.write(f"Codigos: {codigos}\n")
+            file.write(f"Codigos unicos: {codigos_unicos}")
+    return f"Extracción exitosa✅"
+
+extraer_vulnerabilidades("ejercicios_repaso_FINAL\\escaneo.txt")
